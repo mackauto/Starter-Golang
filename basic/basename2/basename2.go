97744236f924b41bd03eccc54ab0fe0c174599f9
JPMike <jpmikemessage@gmail.com>
2019-05-12 18:40:35 +0800

    string and constant example

---
+++ basic/basename2/basename2.go
@@ -0,0 +1,15 @@
+package basename2
+
+import "strings"
+
+// how to use strings package
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+
+}
---
+++ basic/netflag/netflag.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Flags uint
+
+// how to use const and enumeration
+const (
+	FlagUp Flags = 1 << iota
+	FlagBroadcast
+	FlagLoopback
+	FlagPointToPoint
+	FlagMulticast
+)
+
+func IsUp(v Flags) bool {
+	return v&FlagUp == FlagUp
+}
+
+func TurnDown(v *Flags) {
+	*v &^= FlagUp
+}
+
+func SetBroadcast(v *Flags) {
+	*v |= FlagBroadcast
+}
+
+func IsCast(v Flags) bool {
+	return v&(FlagBroadcast|FlagMulticast) != 0
+}
+
+func main() {
+	var v Flags = FlagMulticast | FlagUp
+	fmt.Printf("%b %t\n", v, IsUp(v))
+	TurnDown(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v))
+	SetBroadcast(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v))
+	fmt.Printf("%b %t\n", v, IsCast(v))
+
+}
