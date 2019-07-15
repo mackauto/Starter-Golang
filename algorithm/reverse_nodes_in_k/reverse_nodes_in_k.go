d3dbff0f55f892c1dac0bee7852adb32e82d6a4a
JPMike <jpmikemessage@gmail.com>
2019-07-15 20:57:23 +0800

    reverser nodes in k: temp commit

--- algorithm/reverse_nodes_in_k/reverse_nodes_in_k.go
+++ algorithm/reverse_nodes_in_k/reverse_nodes_in_k.go
@@ -9,9 +9,24 @@ func reverseKGroup(head *util.ListNode, k int) *util.ListNode {
 }
 
 func reverseListNode(start *util.ListNode, end *util.ListNode) *util.ListNode {
-	return &util.ListNode{}
+	prev := end
+	for start != end {
+		next := start.Next
+		start.Next = prev
+		prev = start
+		start = next
+	}
+	return prev
 }
 
 func main() {
-
+	listNodeLen := 5
+	head := util.GetOrderedListNode(listNodeLen)
+	util.PrintListNode(head)
+	end := head
+	for i := 1; i < listNodeLen; i++ {
+		end = end.Next
+	}
+	head = reverseListNode(head, end)
+	util.PrintListNode(head)
 }
