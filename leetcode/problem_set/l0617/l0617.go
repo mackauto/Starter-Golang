899129202cb82e2dae20751ef6d1453abc7aeaf3
JPMike <jpmikemessage@gmail.com>
2019-07-18 11:39:26 +0800

    merge binary tree

---
+++ leetcode/problem_set/l0617/l0617.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/JPMike/Starter-Golang/leetcode/util"
+)
+
+func mergeTrees(t1 *util.TreeNode, t2 *util.TreeNode) *util.TreeNode {
+	if t1 == nil {
+		return t2
+	}
+	if t2 == nil {
+		return t1
+	}
+	t1.Val += t2.Val
+	t1.Left = mergeTrees(t1.Left, t2.Left)
+	t1.Right = mergeTrees(t1.Right, t2.Right)
+	return t1
+}
+
+func main() {
+	t1 := util.GetExampleTree()
+	t1.PrintMidOrder()
+	fmt.Println()
+	t2 := util.GetExampleTree()
+	t := mergeTrees(t1, t2)
+	t.PrintMidOrder()
+}
