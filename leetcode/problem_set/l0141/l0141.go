dc63e61042ba5bc0892d6c7534e873236f52edc8
JPMike <jpmikemessage@gmail.com>
2019-07-23 14:45:49 +0800

    leetcode l0141

---
+++ leetcode/problem_set/l0141/l0141.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	node1 := &ListNode{Val: 3}
+	node2 := &ListNode{Val: 2}
+	node3 := &ListNode{Val: 0}
+	node4 := &ListNode{Val: -4}
+	node1.Next = node2
+	node2.Next = node3
+	node3.Next = node4
+	node4.Next = node2
+	fmt.Println(hasCycle(node1))
+}
