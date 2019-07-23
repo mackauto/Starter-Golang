d4f5ad3a4593bd067bc4ac0eeeca98e4f474f589
JPMike <jpmikemessage@gmail.com>
2019-07-23 20:49:42 +0800

    leetcode l0215

---
+++ leetcode/problem_set/l0215/l0215.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func findKthLargest(nums []int, k int) int {
+	sort.Ints(nums)
+	return nums[len(nums)-k]
+}
+
+func main() {
+	nums := []int{3, 2, 1, 5, 6, 4}
+	fmt.Println(findKthLargest(nums, 2))
+}
