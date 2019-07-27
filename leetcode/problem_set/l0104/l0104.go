5b084230962ffd43c502172ad3ca9c29ce8f1283
JPMike <jpmikemessage@gmail.com>
2019-07-27 16:32:14 +0800

    leetcode l0104

---
+++ leetcode/problem_set/l0104/l0104.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func main() {
+	t := &TreeNode{Val: 3,
+		Left: &TreeNode{Val: 9},
+		Right: &TreeNode{Val: 20,
+			Left:  &TreeNode{Val: 15},
+			Right: &TreeNode{Val: 7}}}
+	fmt.Println(maxDepth(t))
+}
